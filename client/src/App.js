f9975e5d20d658430667545cef82332e8f2dcff6
franesucic <115322660+franesucic@users.noreply.github.com>
2023-11-08 19:05:29 +0100

    Sql injection fixes

--- client/src/App.js
+++ client/src/App.js
@@ -16,25 +16,6 @@ const handleCsrfButton = (e) => {
   setCsrf(!csrf)
 }
 
-/*const handleSubmit = async (e) => {
-  if (!sqlInj) {
-    const regex = /^[a-zA-Z0-9]+$/;   // username i password mogu biti samo kombinacija slova i brojki
-    if (regex.test(username) && regex.test(password)) {
-      let response = await fetch(`http://localhost:3000/sqlon/${username}/${password}`);
-      let jsonData = await response.json();
-      console.log(jsonData)
-      alert(JSON.stringify(jsonData))
-    } else {
-      alert("Neispravan unos.");
-    }
-  } else {
-    let response = await fetch(`http://localhost:3000/sqloff/${username}/${password}`);
-    let jsonData = await response.json();
-    console.log(jsonData)
-    alert(JSON.stringify(jsonData))
-  }
-}*/
-
 const handleSubmit = async (e) => {
   if (sqlInj) {
     let response = await fetch(`http://localhost:3000/sqlon/${username}/${password}`);
@@ -71,7 +52,7 @@ const handlePasswordChange = (e) => {
       <div className="task-div">
         { sqlInj ? <button onClick={handleSqlButton}>Isključi ranjivost</button> : <button onClick={handleSqlButton}>Uključi ranjivost</button> }
         <div className="action-div">
-          <div><b>Dohvati svoje podatke:</b> (Za testiranje ranjivosti unjeti navedeni tekst u neko od polja: a' or '1'='1)</div>
+          <div><b>Dohvati svoje podatke:</b> (Za testiranje ranjivosti unjeti navedeni tekst u polje za lozinku: <span style={{backgroundColor:"red"}}>a' or '1'='1</span>)</div>
           <span>Korisničko ime: </span><input type="text" onChange={handleUsernameChange}></input><span>Lozinka: </span><input type="password" onChange={handlePasswordChange}></input><button onClick={handleSubmit}>Unesi</button>
         </div>
       </div>
--- server/server.js
+++ server/server.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-//const pool = require("./db.js");
+const pool = require("./db.js");
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/", (req, res) => {
-    res.send("Server active.");
-})
-/*
 app.get("/sqlon/:username/:password", async (req, res) => {
     let username = req.params.username;
     let password = req.params.password;
@@ -27,4 +23,4 @@ app.get("/sqloff/:username/:password", async (req, res) => {
 
 app.listen(3000, () => {
     console.log("Server has started on port 3000.");
-})*/
+})
