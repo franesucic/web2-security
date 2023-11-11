e4891196611effb0ee9058159e1933a170111632
franesucic <115322660+franesucic@users.noreply.github.com>
2023-11-11 10:40:03 +0100

    No cors setup

--- client/src/Home.js
+++ client/src/Home.js
@@ -27,7 +27,10 @@ const handleCsrfButton = (e) => {
 
 const handleSubmit = async (e) => {
   if (sqlInj) {
-    let response = await fetch(`https://web2-security-server.onrender.com/sqlon/${username}/${password}`);
+    let response = await fetch(`https://web2-security-server.onrender.com/sqlon/${username}/${password}`, {
+      mode: "no-cors",
+      method: "GET"
+    });
     let jsonData = await response.json();
     if (jsonData.length < 1) alert("Pogrešan unos")
     else {
@@ -39,7 +42,10 @@ const handleSubmit = async (e) => {
       alert(string);
     }
   } else {
-    let response = await fetch(`https://web2-security-server.onrender.com/sqloff/${username}/${password}`);
+    let response = await fetch(`https://web2-security-server.onrender.com/sqloff/${username}/${password}`, {
+      mode: "no-cors",
+      method: "GET"
+    });
     let jsonData = await response.json();
     if (jsonData.length === 0) {
       alert("Pogrešan unos");
