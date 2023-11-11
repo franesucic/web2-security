818a330aca5b320c19988009d0213fd2fe431ab2
franesucic <115322660+franesucic@users.noreply.github.com>
2023-11-11 11:48:22 +0100

    Changes on client

--- client/src/Home.js
+++ client/src/Home.js
@@ -27,9 +27,7 @@ const handleCsrfButton = (e) => {
 
 const handleSubmit = async (e) => {
   if (sqlInj) {
-    let response = await fetch(`https://web2-security-server.onrender.com/sqlon/${username}/${password}`, {
-      mode: "no-cors"
-    });
+    let response = await fetch(`https://web2-security-server.onrender.com/sqlon/${username}/${password}`);
     let jsonData = await response.json();
     if (jsonData.length < 1) alert("Pogrešan unos")
     else {
@@ -41,9 +39,7 @@ const handleSubmit = async (e) => {
       alert(string);
     }
   } else {
-    let response = await fetch(`https://web2-security-server.onrender.com/sqloff/${username}/${password}`, {
-      mode: "no-cors"
-    });
+    let response = await fetch(`https://web2-security-server.onrender.com/sqloff/${username}/${password}`);
     let jsonData = await response.json();
     if (jsonData.length === 0) {
       alert("Pogrešan unos");
